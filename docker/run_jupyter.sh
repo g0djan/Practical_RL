702451604ffe2b14bac87073c3a220772686016f
godjan <g0dj4n@gmail.com>
2025-07-11 21:40:13 +0000

    Image with CUML

--- docker/run_jupyter.sh
+++ docker/run_jupyter.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-jupyter notebook --no-browser --ip 0.0.0.0
+
+jupyter notebook password
+
+jupyter notebook \
+  --ip=0.0.0.0 \
+  --port=8888 \
+  --no-browser \
+  --certfile=/certs/mycert.pem \
+  --keyfile=/certs/mykey.key
